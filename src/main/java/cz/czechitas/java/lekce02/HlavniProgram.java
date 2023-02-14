43d3f02784cb46d011575d0e46c47469c386a953
Jan Žanta <honza.zanta@email.cz>
2023-02-14 20:13:32 +0100

    domeček a prasátko2

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -11,37 +11,43 @@ public class HlavniProgram {
             zofka.turnRight(90);
             zofka.move(180);}
 
+        // střecha domu
         zofka.turnRight(45);
         zofka.move(130);
         zofka.turnRight(90);
         zofka.move(130);
 
+        //posunutí k nakreslení prasátka
         zofka.penUp();
         zofka.turnRight(135);
-        zofka.move(360);
+        zofka.move(480);
         zofka.penDown();
 
         for (int i = 0; i < 4; i++) {
             zofka.turnRight(90);
             zofka.move(180);}
 
+        //střecha domu
         zofka.turnRight(45);
         zofka.move(130);
         zofka.turnRight(90);
         zofka.move(130);
 
-
+        //1. noha prasátka
         zofka.turnLeft(90);
         zofka.move(40);
-        zofka.turnRight(180);
+        zofka.turnLeft(180);
         zofka.move(40);
         zofka.turnLeft(90);
         zofka.move(40);
         zofka.turnLeft(180);
         zofka.move(40);
+
+        //přesunutí mezi nohami
         zofka.turnLeft(135);
         zofka.move(180);
 
+        // 2. noha prasátka
         zofka.turnLeft(135);
         zofka.move(40);
         zofka.turnRight(180);
@@ -50,6 +56,7 @@ public class HlavniProgram {
         zofka.move(40);
         zofka.turnLeft(180);
         zofka.move(40);
+
 
     }
 
