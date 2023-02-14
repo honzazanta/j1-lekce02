e7bcf93b247861482cef70293b3653fe0412ee46
Jan Žanta <honza.zanta@email.cz>
2023-02-14 20:04:23 +0100

    domeček a prasátko

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -7,7 +7,50 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
-        //TODO tady bude kód
+        for (int i = 0; i < 4; i++) {
+            zofka.turnRight(90);
+            zofka.move(180);}
+
+        zofka.turnRight(45);
+        zofka.move(130);
+        zofka.turnRight(90);
+        zofka.move(130);
+
+        zofka.penUp();
+        zofka.turnRight(135);
+        zofka.move(360);
+        zofka.penDown();
+
+        for (int i = 0; i < 4; i++) {
+            zofka.turnRight(90);
+            zofka.move(180);}
+
+        zofka.turnRight(45);
+        zofka.move(130);
+        zofka.turnRight(90);
+        zofka.move(130);
+
+
+        zofka.turnLeft(90);
+        zofka.move(40);
+        zofka.turnRight(180);
+        zofka.move(40);
+        zofka.turnLeft(90);
+        zofka.move(40);
+        zofka.turnLeft(180);
+        zofka.move(40);
+        zofka.turnLeft(135);
+        zofka.move(180);
+
+        zofka.turnLeft(135);
+        zofka.move(40);
+        zofka.turnRight(180);
+        zofka.move(40);
+        zofka.turnLeft(90);
+        zofka.move(40);
+        zofka.turnLeft(180);
+        zofka.move(40);
+
     }
 
     public static void main(String[] args) {
